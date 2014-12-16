26d2b4694f26ecd534e2ba38739d857ecb334bff
IceHornet <f.ulmschneider@bluewin.ch>
2014-12-16 18:51:23 +0100

    Added sleep mode

--- wuerfel_test/wuerfel_test.ino
+++ wuerfel_test/wuerfel_test.ino
@@ -1,10 +1,14 @@
 #include <Entropy.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
 
 const byte led1 = 0;
 const byte led2 = 3;
 const byte led3 = 4;
 const byte led4 = 1;
 const byte swPin = 2;
+const byte emptyPin = 5;
 byte randNum;
 byte prevNum;
 
@@ -16,6 +20,10 @@ void setup() {
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
   pinMode(swPin, INPUT_PULLUP);
+  pinMode(emptyPin, INPUT_PULLUP);
+	
+  power_adc_disable();
+  power_usi_disable();
   
   for(byte i=0; i<50; i++) {
     digitalWrite(led1, HIGH);
@@ -40,16 +48,16 @@ void loop() {
   if (digitalRead(swPin) == LOW) {
     
     for(byte i=1; i<7; i++) {
-		
+    
       digitalWrite(led1, LOW);
       digitalWrite(led2, LOW);
       digitalWrite(led3, LOW);
       digitalWrite(led4, LOW);
       
       do {
-			
+      
         randNum=Entropy.random(0,6);
-				
+        
       } while(randNum == prevNum);
     
       disp(randNum);
@@ -58,38 +66,55 @@ void loop() {
       delay(i*50);
     }
   }
+  sleepNow();
 }
 
 void disp(byte x) {
-	switch(x) { 
-		case 0:
-			digitalWrite(led4, HIGH);
-		break;
-		
-		case 1:
-			digitalWrite(led3, HIGH);
-		break;
-		
-		case 2:
-			digitalWrite(led3, HIGH);
-			digitalWrite(led4, HIGH);
-		break;
-		
-		case 3:
-			digitalWrite(led1, HIGH);
-			digitalWrite(led3, HIGH);
-		break;
-		
-		case 4:
-			digitalWrite(led1, HIGH);
-			digitalWrite(led3, HIGH);
-			digitalWrite(led4, HIGH);
-		break;
-		
-		case 5:
-			digitalWrite(led1, HIGH);
-			digitalWrite(led2, HIGH);
-			digitalWrite(led3, HIGH);
-		break;
-	}
+  switch(x) { 
+    case 0:
+      digitalWrite(led4, HIGH);
+    break;
+    
+    case 1:
+      digitalWrite(led3, HIGH);
+    break;
+    
+    case 2:
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+    break;
+    
+    case 3:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led3, HIGH);
+    break;
+    
+    case 4:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+    break;
+    
+    case 5:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+    break;
+  }
+}
+void sleepNow(void) {
+  attachInterrupt(swPin, pinInterrupt, LOW);
+  delay(100);
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+  sleep_enable();
+
+  sleep_mode();
+
+  sleep_disable();
+}
+
+void pinInterrupt(void) {
+  detachInterrupt(swPin);
 }
