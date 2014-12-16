ea2582aab81b81cd8a72d9db97c63993d8f40fbc
IceHornet <f.ulmschneider@bluewin.ch>
2014-12-16 18:06:27 +0100

    Cleaned up the code

--- wuerfel_test/wuerfel_test.ino
+++ wuerfel_test/wuerfel_test.ino
@@ -5,12 +5,10 @@ const byte led2 = 3;
 const byte led3 = 4;
 const byte led4 = 1;
 const byte swPin = 2;
-//const int analogPin =0;
 byte randNum;
 byte prevNum;
 
 void setup() {
-  //Serial.begin(9600);
   Entropy.initialize();
   
   pinMode(led1, OUTPUT);
@@ -18,8 +16,6 @@ void setup() {
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
   pinMode(swPin, INPUT_PULLUP);
-  
-  //randomSeed(analogRead(analogPin));
   
   for(byte i=0; i<50; i++) {
     digitalWrite(led1, HIGH);
@@ -44,17 +40,17 @@ void loop() {
   if (digitalRead(swPin) == LOW) {
     
     for(byte i=1; i<7; i++) {
-      
+		
       digitalWrite(led1, LOW);
       digitalWrite(led2, LOW);
       digitalWrite(led3, LOW);
       digitalWrite(led4, LOW);
       
       do {
+			
         randNum=Entropy.random(0,6);
-        //Serial.print(randNum);
+				
       } while(randNum == prevNum);
-      //Serial.println(randNum);
     
       disp(randNum);
       
@@ -65,35 +61,35 @@ void loop() {
 }
 
 void disp(byte x) {
-  switch(x) { 
-        case 0:
-          digitalWrite(led4, HIGH);
-        break;
-        
-        case 1:
-          digitalWrite(led3, HIGH);
-        break;
-        
-        case 2:
-          digitalWrite(led3, HIGH);
-          digitalWrite(led4, HIGH);
-        break;
-        
-        case 3:
-          digitalWrite(led1, HIGH);
-          digitalWrite(led3, HIGH);
-        break;
-        
-        case 4:
-          digitalWrite(led1, HIGH);
-          digitalWrite(led3, HIGH);
-          digitalWrite(led4, HIGH);
-        break;
-        
-        case 5:
-          digitalWrite(led1, HIGH);
-          digitalWrite(led2, HIGH);
-          digitalWrite(led3, HIGH);
-        break;
-      }
+	switch(x) { 
+		case 0:
+			digitalWrite(led4, HIGH);
+		break;
+		
+		case 1:
+			digitalWrite(led3, HIGH);
+		break;
+		
+		case 2:
+			digitalWrite(led3, HIGH);
+			digitalWrite(led4, HIGH);
+		break;
+		
+		case 3:
+			digitalWrite(led1, HIGH);
+			digitalWrite(led3, HIGH);
+		break;
+		
+		case 4:
+			digitalWrite(led1, HIGH);
+			digitalWrite(led3, HIGH);
+			digitalWrite(led4, HIGH);
+		break;
+		
+		case 5:
+			digitalWrite(led1, HIGH);
+			digitalWrite(led2, HIGH);
+			digitalWrite(led3, HIGH);
+		break;
+	}
 }
